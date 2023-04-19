8e4606bfd31ade725ac7cd411dc7c86f32dab4ab
T-turbo-Pascal <70170383+T-turbo-Pascal@users.noreply.github.com>
2023-04-19 14:14:43 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -2,30 +2,14 @@
 import Header from './components/header';
 import Card from './components/card';
 import Footer from './components/footer';
-import MainPage from './pages/main';
+import Mainpage from './pages/Mainpage';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Mainpage />
       <Card />
-      <div className="flex flex-col items-center justify-center h-screen">
-        <h1 className="text-4xl font-bold mb-8">HOW TO PLAY?</h1>
-        <div className="flex justify-center space-x-8">
-          <div className="w-80 p-4 flex items-center bg-opacity-75 bg-blue-900">
-            <p className="text-white font-bold text-8xl mr-4">1</p>
-            <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
-          </div>
-          <div className="w-80 p-4 flex items-center bg-opacity-75 bg-blue-900">
-            <p className="text-white font-bold text-8xl mr-4">2</p>
-            <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
-          </div>
-          <div className="w-80 p-4 flex items-center bg-opacity-75 bg-blue-900">
-            <p className="text-white font-bold text-8xl mr-4">3</p>
-            <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
-          </div>
-        </div>
-      </div>
       <Footer />
     </div>
   );
---
+++ src/pages/Mainpage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function Mainpage() {
+    return (
+        //Promo block
+        
+
+        // HOW TO PLAY? blcok
+        <div className="flex flex-col items-center justify-center h-screen">
+            <h1 className="text-4xl font-bold mb-8">HOW TO PLAY?</h1>
+            <div className="flex justify-center space-x-8">
+                <div className="w-80 p-4 flex items-center bg-opacity-75 bg-blue-900">
+                    <p className="text-white font-bold text-8xl mr-4">1</p>
+                    <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
+                </div>
+                <div className="w-80 p-4 flex items-center bg-opacity-75 bg-blue-900">
+                    <p className="text-white font-bold text-8xl mr-4">2</p>
+                    <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
+                </div>
+                <div className="w-80 p-4 flex items-center bg-opacity-75 bg-blue-900">
+                    <p className="text-white font-bold text-8xl mr-4">3</p>
+                    <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Mainpage
