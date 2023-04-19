332be79b7788cfe6759ffa76304e5abb37ff0418
T-turbo-Pascal <70170383+T-turbo-Pascal@users.noreply.github.com>
2023-04-19 17:32:15 +0200

    replace card data

--- src/components/card.js
+++ src/components/card.js
@@ -1,45 +1,39 @@
 import React from "react";
 
-const data = [
-    {
-      title: 'Syper loto',
-      date: 'Created on: 19th April 2023',
-      description: 'Text',
-      price : 'Price: $9.99'
-    }
-  ];
+const Card = ({ data }) => {
+    return (
+        <div className="flex flex-col items-center md:flex-row md:max-w-xl bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow">
+            <img
+                className="object-cover w-full h-96 md:h-auto md:w-48 md:rounded-l-lg md:rounded-t-none rounded-t-lg"
+                src={data.img}
+                alt={data.alt}
+                style={{ backgroundColor: "rgba(11, 18, 29, 0.7)", width: "229px", height: "210px" }}
+            />
 
-
-const Card = () => {
-  return (
-    <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl dark:border-gray-700 dark:bg-gray-800 ">
-      <img
-        className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg"
-        src="/img/card/1.png"
-        alt=""
-      />
-      <div className="flex flex-col justify-between p-4 leading-normal">
-        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-          {data[0].title}
-        </h5>
-        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
-          {data[0].description}
-        </p>
-        <div className="flex justify-between items-center">
-          <p className="text-gray-500 text-sm">{data[0].date}</p>
-          <p className="text-gray-500 text-sm">{data[0].price}</p>
+            <div className="flex flex-col justify-between p-4 leading-normal md:flex-grow">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                    {data.title}
+                </h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+                    {data.description}
+                    {data.date}
+                    {data.price}
+                </p>
+                <div className="flex justify-between items-center md:items-end">
+                    <p className="text-gray-500 text-sm">{data.date}</p>
+                    <p className="text-gray-500 text-sm">{data.price}</p>
+                </div>
+                <div className="flex justify-between items-center mt-4 md:mt-0">
+                    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md">
+                        About
+                    </button>
+                    <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded-md">
+                        Play Now
+                    </button>
+                </div>
+            </div>
         </div>
-        <div className="flex justify-between items-center">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            About
-          </button>
-          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Play Now
-          </button>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Card;
--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -1,26 +1,74 @@
 import React from 'react'
+import Card from '../components/card';
+
+const data = [
+    {
+        title: '$448,000,000',
+        date: '26 April 19:00',
+        description: 'Text',
+        price: '$5.00',
+        img: '/img/card/1.png',
+        alt: 'image of card'
+    },
+    {
+        title: '$448,000,000',
+        date: '26 April 19:00',
+        description: 'Text',
+        price: '$5.00',
+        img: '/img/card/3.png',
+        alt: 'image of card'
+    },
+    {
+        title: '$448,000,000',
+        date: '26 April 19:00',
+        description: 'Text',
+        price: '$5.00',
+        img: '/img/card/2.png',
+        alt: 'image of card'
+    },
+    {
+        title: '$448,000,000',
+        date: '26 April 19:00',
+        description: 'Text',
+        price: '$5.00',
+        img: '/img/card/4.png',
+        alt: 'image of card'
+    },
+];
+
 function Mainpage() {
     return (
-        //Promo block
-        
-
-        // HOW TO PLAY? blcok
-        <div className="flex flex-col items-center justify-center h-screen space-y-8">
-            <h1 className="text-4xl font-bold text-white mb-8 ">HOW TO PLAY?</h1>
-            <div className="flex justify-center space-x-8">
-                <div className="w-80 p-4 flex items-center bg-opacity-75 bg-zinc-900 rounded-lg">
-                    <p className="text-white font-bold text-8xl mr-4">1</p>
-                    <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
-                </div>
-                <div className="w-80 p-4 flex items-center bg-opacity-75 bg-zinc-900 rounded-lg">
-                    <p className="text-white font-bold text-8xl mr-4">2</p>
-                    <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
-                </div>
-                <div className="w-80 p-4 flex items-center bg-opacity-75 bg-zinc-900 rounded-lg">
-                    <p className="text-white font-bold text-8xl mr-4">3</p>
-                    <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
+        <div>
+            {/* HOW TO PLAY? blcok */}
+            <div className="flex flex-col items-center justify-center h-screen space-y-8">
+                <h1 className="text-4xl font-bold text-white mb-8 ">HOW TO PLAY?</h1>
+                <div className="flex justify-center space-x-8">
+                    <div className="w-80 p-4 flex items-center bg-opacity-75 bg-zinc-900 rounded-lg">
+                        <p className="text-white font-bold text-8xl mr-4">1</p>
+                        <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
+                    </div>
+                    <div className="w-80 p-4 flex items-center bg-opacity-75 bg-zinc-900 rounded-lg">
+                        <p className="text-white font-bold text-8xl mr-4">2</p>
+                        <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
+                    </div>
+                    <div className="w-80 p-4 flex items-center bg-opacity-75 bg-zinc-900 rounded-lg">
+                        <p className="text-white font-bold text-8xl mr-4">3</p>
+                        <p className="text-white">Pick a lottery and your lucky numbers to participate in the draw</p>
+                    </div>
                 </div>
             </div>
+            {/* Play block */}
+            <div className="container mx-auto p-4">
+                <h1 className="text-4xl font-bold mb-4 text-white text-center">PLAY LOTTERY ONLINE</h1>
+                <div className="grid grid-cols-2 gap-4 justify-center">
+                    {data.map((item, index) => (
+                        <div className="col-span-1 mb-4" key={index}>
+                            <Card data={item} />
+                        </div>
+                    ))}
+                </div>
+            </div>
+
         </div>
     )
 }
