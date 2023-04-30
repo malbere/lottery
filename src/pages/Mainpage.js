0a21f9515d6faece31b63aa0d0007f9ad76a4eb5
Anton <61839404+malberecode@users.noreply.github.com>
2023-04-30 16:46:01 +0200

    mainpage edit close to figma

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -48,16 +48,29 @@ function Mainpage() {
     return (
         <div>
 
-            <div className="flex ">
-                <div className="w-1/2 pl-4">
-                    <p className="text-white text-8xl w-screen ml-auto pl-12 mb-10 font-extrabold">Make Your Own Luck!</p>
-                    <p className="text-white text-2xl ml-auto pl-12 mb-0">Believe it or not... you're luckier</p> <br /> <p className="text-white text-2xl ml-auto pl-12 mt-0">than you think!</p>
-                    <div className="pl-12 pt-20">
-                        <button type="button" onClick={handleGetStartedClick} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+            <div class="container px-16 pl-20">
+                <div class="flex flex-col md:flex-row pl-20">
+                    <div class="w-full md:w-1/2 pl-20">
+                        <p class="text-white text-8xl ml-auto pl-12 mb-10 font-extrabold">
+                            Make Your Own Luck!
+                        </p>
+                        <p class="text-white text-2xl ml-auto pl-12 mb-0">
+                            Believe it or not... you're luckier
+                        </p>
+                        <p class="text-white text-2xl ml-auto pl-12 mt-0">than you think!</p>
+                        <div class="pl-12 pt-20">
+                            <button
+                                type="button"
+                                onClick={handleGetStartedClick}
+                                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                            >
+                                Get started
+                            </button>
+                        </div>
                     </div>
-                </div>
-                <div className="w-1/2">
-                    <img className="float-right" src="img/promo.png" alt="" />
+                    <div class="w-1/2 flex justify-center items-center pl-10">
+                        <img class="w-full" src="img/promo.png" alt="" />
+                    </div>
                 </div>
             </div>
             <div className="flex flex-col items-center justify-center space-y-8 mt-20 mb-20">
@@ -84,7 +97,7 @@ function Mainpage() {
                 <h1 className="text-4xl font-bold mb-10 text-white text-center">PLAY LOTTERY ONLINE</h1>
                 <div className="grid grid-cols-2 gap-4 justify-center">
                     {data.map((item, index) => (
-                        <div className="col-span-1 mb-4" key={index}>
+                        <div className="col-span-1 mb-4 pl-24" key={index}>
                             <Card data={item} />
                         </div>
                     ))}
