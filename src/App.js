49d6746a938f3ad5b4b1d73ab6411b42040b3127
Anton <61839404+malberecode@users.noreply.github.com>
2023-05-01 17:57:54 +0200

    add login page with bd check (no session)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import './App.css';
 import React from 'react';
 import Header from './components/header';
-import Card from './components/card';
 import Footer from './components/footer';
 import Mainpage from './pages/Mainpage';
 
@@ -9,7 +8,6 @@ function App() {
   return (
     <div className="App">
       <Header />
-      {/* <Card/> */}
       <Mainpage />
       <Footer />
     </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios'
 
 function Login() {
     const navigate = useNavigate();
+
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+    
 
     const handleRegister = () => {
         navigate('/register');
@@ -11,6 +16,28 @@ function Login() {
     const handleMain = () => {
         navigate('/');
     };
+
+      const handleSubmit = async (el) => {
+        const response = await axios.get('http://localhost:8800/users');
+        el.preventDefault();
+        let exist = 0;
+        for(let i = 0;i<response.data.length;i++){
+          if(response.data[i].user_name === username){
+              exist = 1;
+          }
+        }
+        if(exist){
+          const check = await axios.get('http://localhost:8800/users', {'username': username, 'password': password});
+          console.log(check);
+          console.log('Logged in!');
+          navigate('/');
+        }
+        else{
+          alert('Wrong username or password');
+          console.log('Wrong username or password');
+        }
+    
+      };
     return (
         <section className="bg-gray-50 dark:bg-gray-900">
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -24,15 +51,15 @@ function Login() {
                         </h1>
                         <form className="space-y-4 md:space-y-6" action="#">
                             <div>
-                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
-                                <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="" />
+                                <label htmlFor="login" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">UserName</label>
+                                <input value={username} onChange={el => setUserName(el.target.value)} type="login" name="login" id="login" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="username" required="" />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
-                                <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
+                                <input value={password} onChange={el => setPassword(el.target.value)} type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="" />
                             </div>
                             <div>
-                                <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <button onClick={handleSubmit} type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-primary-600 hover:bg-primary-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-500 dark:bg-primary-500 dark:hover:bg-primary-600 dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                     Log in
                                 </button>
                             </div>
