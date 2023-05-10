f0c888072e3a05397993de3528f2dc6269a2eb26
T-turbo-Pascal <70170383+T-turbo-Pascal@users.noreply.github.com>
2023-05-10 11:23:51 +0200

    fix some design and rewrite logic of showAllLotteries button

--- src/pages/Mainpage.js
+++ src/pages/Mainpage.js
@@ -70,7 +70,17 @@ const data = [
         alt: 'image of card'
     }
 ];
+const maxLotteries = 6;
 
+function SkeletonCard() {
+    return (
+      <div className="bg-white p-6 rounded-md shadow-md animate-pulse">
+        <div className="h-4 bg-gray-400 rounded mb-4"></div>
+        <div className="h-8 bg-gray-400 rounded mb-4"></div>
+        <div className="h-8 bg-gray-400 rounded"></div>
+      </div>
+    );
+  }
 
 function Mainpage() {
     const navigate = useNavigate();
@@ -88,9 +98,9 @@ function Mainpage() {
     return (
         <div>
 
-            <div class="" style={{background: 'linear-gradient(83.54deg, rgba(0, 0, 0, 0.49) 4.49%, rgba(0, 0, 0, 0.3332) 51.63%, rgba(0, 0, 0, 0) 94.46%)', height: 883,}}>
+            <div class="" style={{ background: 'linear-gradient(83.54deg, rgba(0, 0, 0, 0.49) 4.49%, rgba(0, 0, 0, 0.3332) 51.63%, rgba(0, 0, 0, 0) 94.46%)', height: 883, }}>
                 <div class="flex">
-                    <div class="w-full md:w-1/2 pl-22" style={{paddingTop: "230px", paddingLeft: '375px'}}>
+                    <div class="w-full md:w-1/2 pl-22" style={{ paddingTop: "230px", paddingLeft: '375px' }}>
                         <p class="text-white text-8xl ml-auto pl-12 mb-10 font-extrabold" >
                             Make Your Own<br></br> Luck!
                         </p>
@@ -108,13 +118,13 @@ function Mainpage() {
                             </button>
                         </div>
                     </div>
-                    <div style={{ position: 'relative', width: '1200px', height: '883px'}} >
-                        <img class="w-full" src="img/promopng.png" alt="" style={{ margingRight: "-100px", position: 'absolute', bottom: 0, right: 0,}}/>
+                    <div style={{ position: 'relative', width: '1200px', height: '883px' }} >
+                        <img class="w-full" src="img/promopng.png" alt="" style={{ margingRight: "-100px", position: 'absolute', bottom: 0, right: 0, }} />
                     </div>
                 </div>
             </div>
             <div className="flex flex-col items-center justify-center space-y-8 mt-20 mb-20">
-                <h1 className="text-4xl font-bold text-white mb-8 " style={{fontStyle: "normal", lineHeight: "140%", fontWeight: 700, fontSize: "42px"}}>HOW TO PLAY?</h1>
+                <h1 className="text-4xl font-bold text-white mb-8 " style={{ fontStyle: "normal", lineHeight: "140%", fontWeight: 700, fontSize: "42px" }}>HOW TO PLAY?</h1>
                 <div className="flex justify-center space-x-8">
                     <div className="w-80 p-4 flex items-center bg-opacity-75 bg-zinc-900 rounded-lg">
 
@@ -133,19 +143,33 @@ function Mainpage() {
 
                 </div>
             </div >
-            <div style={{backgroundColor: "#0B121D"}}>
+            <div style={{ backgroundColor: "#0B121D" }}>
                 < div className="container mx-auto p-10 pt-12" >
-                    <h1 className="text-4xl font-bold mb-10 text-white text-center" style={{fontStyle: "normal", fontWeight: 700, fontSize: "42px", lineHeight: "140%", marginBlockEnd: "10%", paddingTop: "3%", backgroundColor: "#0B121D"}}>PLAY LOTTERY ONLINE</h1>
-                    <div className="grid grid-cols-2 gap-4 justify-center"> 
-                        {data.map((item, index) => (
-                            <div className="col-span-1 mb-4 pl-24" key={index}>
-                                <Card data={item} />
-                            </div>
-                        ))}
-                    </div>
+                    <h1 className="text-4xl font-bold mb-10 text-white text-center" style={{ fontStyle: "normal", fontWeight: 700, fontSize: "42px", lineHeight: "140%", marginBlockEnd: "10%", paddingTop: "3%", backgroundColor: "#0B121D" }}>PLAY LOTTERY ONLINE</h1>
                     <div>
                         {!showAllLotteries && (
-                            <button onClick={handleViewAllLotteries}>View all lotteries</button>
+                            <div className="grid grid-cols-2 gap-4 justify-center">
+                                {data.slice(0, maxLotteries).map((item, index) => (
+                                    <div className="col-span-1 mb-4 pl-24" key={index}>
+                                        <Card data={item} />
+                                    </div>
+                                ))}
+                                <div style={{
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}>
+                                    <button style={{
+                                        color: 'rgba(255, 255, 255, 0.7)',
+                                        display: 'flex',
+                                        justifyContent: 'center',
+                                        alignItems: 'center',
+                                    }} onClick={handleViewAllLotteries}>
+                                        View all lotteries
+                                        <img src="img/arrow-down.svg" alt="arrow down" style={{ marginLeft: '8px' }} />
+                                    </button>
+                                </div>
+                            </div>
                         )}
                         {showAllLotteries && (
                             <div className="grid grid-cols-2 gap-4 justify-center">
